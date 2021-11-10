f56119c0d139ba1174b61c740699e3ac6de5995e
beslerpatryk <beslerpatryk@gmail.com>
2021-11-10 21:07:42 +0800

    feat: add translation file

---
+++ cn-translation.js
@@ -0,0 +1,148 @@
+
+var translationJSON = {
+    "custom_lang_switcher_container_id": "translateMenuContainer",
+    // "custom_lang_switcher_html": 
+    //     `<span class=\"notranslate\"><a href=\"http://www.yourdomain.com/index.html\">ENG</a> |
+    //                                  <a href=\"http://es.yourdomain.com/index.html\">ESP</a> | 
+    //                                  <a href=\"http://fr.yourdomain.com/index.html\">FRA</a>
+    //                                  </span>`,
+
+    "languages": [
+        {
+            "lang_name": "En",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "中",
+            "lang_code": "cn",
+            "url_pattern": "?"
+        }
+    ],
+    "translated_segments": [
+        {
+            "source": "Patryk Besler - Frontend Developer",
+            "target": "Patryk Besler - 前端开发员"
+        },
+        {
+            "source": "Projects",
+            "target": "项目"
+        },
+        {
+            "source": "Technologies",
+            "target": "技术"
+        },
+        {
+            "source": "About",
+            "target": "关于"
+        },{
+            "source": "Hi there,<br>I'm Patrick.",
+            "target": "Hi there,<br>我是 Patrick"
+        },{
+            "source": "Software Engineering student with a flair for design, devoted to building quality websites and apps.",
+            "target": "我是软件工程专业的在校大学生，擅长软件设计，致力于构建高质量的网站和应用程序"
+        },{
+            "source": "Get in touch",
+            "target": "联系我"
+        },{
+            "source": "Live",
+            "target": "演示"
+        },{
+            "source": "Code",
+            "target": "演示"
+        },{
+            "source": "Time Tracking Dashboard",
+            "target": "时间跟踪仪表板"
+        },{
+            "source": "A fully responsive landing page project. As part of the process, I applied a mobile-first workflow and followed semantic HTML5 markup guidelines.",
+            "target": "一个完全响应的登陆页面项目。作为这个过程的一部分，我应用了移动优先的工作流程，并遵循语义学HTML5标记指南。"
+        },{
+            "source": "View the optimal layout for the site depending on their device's screen size",
+            "target": "根据设备的屏幕大小查看网站的最佳布局"
+        },{
+            "source": "See hover states for all interactive elements on the page",
+            "target": "查看页面上所有交互元素的悬停状态"
+        },{
+            "source": "Switch between viewing Daily, Weekly, and Monthly stats",
+            "target": "在查看每日、每周和每月统计资料之间进行切换"
+        },{
+            "source": "A progressive, fully responsive web application to help users discover trending movies. This app features multiple API calls to fetch movie lists and details. Users can also take advantage of the search bar to find any movie they need. Mobile-first workflow was applied together with semantic HTML5 markup guidelines. Built using TMDB API and JavaScript.",
+            "target": "一个渐进的、完全响应的网络应用，帮助用户发现流行的电影。这个应用程序具有多个API调用，以获取电影列表和细节。用户还可以利用搜索栏来寻找他们需要的任何电影。我使用了TMDB API 和 JavaScript 来构建此程序，并应用了移动优先的工作流程与语义学HTML5 标记准则。"
+        },{
+            "source": "Payment App <br> Landing Page",
+            "target": "支付应用着陆页"
+        },{
+            "source": "A fully responsive landing page project. As part of the process, I applied a mobile-first workflow and followed semantic HTML5 markup guidelines. Users are able to:",
+            "target": "一个完全响应的登陆页面项目。作为这个过程的一部分，我应用了移动优先的工作流程，并遵循语义学 HTML5 标记准则"
+        },{
+            "source": "Communicator App <br> Landing Page",
+            "target": "通信器应用程序着陆页"
+        },{
+            "source": "A fully responsive landing page project. As part of the process, I applied a mobile-first workflow and followed semantic HTML5 markup guidelines. Users are able to:",
+            "target": "完全响应的着陆页项目。作为流程的一部分，我应用了移动第一的工作流程，并遵循了语义HTML5标记指南。用户能够："
+        },{
+            "source": "I've worked with a range of technologies in the web development world. From back-end to design...",
+            "target": "我具有使用一系列从设计到后端的技术来实现web开发的经验。"
+        },{
+            "source": "Front-End",
+            "target": "前端"
+        },{
+            "source": "Experience with HTML/CSS and JavaScript ES6",
+            "target": "具有HTML/CSS 和 JavaScript ES6 的基础"
+        },{
+            "source": "Back-End",
+            "target": "后端"
+        },{
+            "source": "Experience with SQL",
+            "target": "具有 SQL 的基础"
+        },{
+            "source": "Experience with tools such as AdobeXD and Figma",
+            "target": "具有使用 AdobeXD 和 Figma 等工具的经验"
+        },{
+            "source": "About me",
+            "target": "关于"
+        },{
+            "source": "I am a student of software engineering, living in Chengdu, China. When I'm not coding, I like to play PC games, learn Chinese, and take silly photos of my cat, Pizza.",
+            "target": "我是一名软件工程专业的学生，住在中国成都。在不写代码的时候，我喜欢玩电脑游戏，学习中文，并为我的猫咪 Pizza 拍一些愚蠢的照片。"
+        },{
+            "source": "Moved to China and started Software Engineering studies",
+            "target": "移居中国，开始学软件工程"
+        },{
+            "source": "Worked as a freelance Graphic Designer",
+            "target": "担任自由职业平面设计师"
+        },{
+            "source": "First webpage project",
+            "target": "第一个网页项目"
+        },{
+            "source": "Expanding my knowledge about Software Engineering",
+            "target": "拓展我对软件工程的知识"
+        },{
+            "source": "Focused on web development career path",
+            "target": "专注于web开发的职业道路"
+        },{
+            "source": "Links to my profiles",
+            "target": "我的 profile 链接"
+        },{
+            "source": "Coding Challenge Websites",
+            "target": "编码挑战网站"
+        },{
+            "source": "Email",
+            "target": "电子邮件"
+        },{
+            "source": "Projects",
+            "target": "项目"
+        },{
+            "source": "Projects",
+            "target": "项目"
+        },{
+            "source": "Projects",
+            "target": "项目"
+        },{
+            "source": "Projects",
+            "target": "项目"
+        },{
+            "source": "Projects",
+            "target": "项目"
+        }
+    ]
+};
